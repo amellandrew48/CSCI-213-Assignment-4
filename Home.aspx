524b058680df9399559667a72516fd5385ee1b67
matsouzasiqueira <123436387+matsouzasiqueira@users.noreply.github.com>
2023-11-14 19:43:50 -0600

    login implementation

--- Home.aspx
+++ Home.aspx
@@ -16,7 +16,7 @@
     <tr>
         <td class="auto-style1">&nbsp;</td>
         <td>
-            <asp:Login ID="Login1" runat="server" BackColor="#E3EAEB" BorderColor="#E6E2D8" BorderPadding="4" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#333333" TextLayout="TextOnTop">
+            <asp:Login ID="Login1" runat="server" OnAuthenticate="Login1_Authenticate" OnLoggedIn="Login1_LoggedIn" BackColor="#E3EAEB" BorderColor="#E6E2D8" BorderPadding="4" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#333333" TextLayout="TextOnTop">
                 <InstructionTextStyle Font-Italic="True" ForeColor="Black" />
                 <LoginButtonStyle BackColor="White" BorderColor="#C5BBAF" BorderStyle="Solid" BorderWidth="1px" Font-Names="Verdana" Font-Size="0.8em" ForeColor="#1C5E55" />
                 <TextBoxStyle Font-Size="0.8em" />
--- Home.aspx.cs
+++ Home.aspx.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Web.Security;
 using System.Web.UI.WebControls;
 
 namespace MSDAssignment4
@@ -11,7 +10,84 @@ namespace MSDAssignment4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            string username = Login1.UserName;
+            string password = Login1.Password;
+
+            if (AuthenticateUser(username, password))
+            {
+                e.Authenticated = true;
+            }
+            else
+            {
+             
+                Login1.FailureText = "Invalid username or password.";
+                e.Authenticated = false;
+            }
+        }
+
+        protected void Login1_LoggedIn(object sender, EventArgs e)
+        {
+            string userType = Session["UserType"] as string;
+
+            switch (userType)
+            {
+                case "Member":
+                    Response.Redirect("Member.aspx");
+                    break;
+                case "Instructor":
+                    Response.Redirect("Instructor.aspx");
+                    break;
+                case "Administrator":  
+                    Response.Redirect("Admin.aspx");
+                    break;
+                default:                   
+                    Response.Redirect("Home.aspx");
+                    break;
+            }
+        }
+
+        private bool AuthenticateUser(string username, string password)
+        {
+            bool authenticated = false;
+            string connectionString = WebConfigurationManager.ConnectionStrings["KarateSchool_1_ConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "SELECT UserType FROM NetUser WHERE UserName = @UserName AND UserPassword = @UserPassword"; 
+
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@UserName", username);
+                        cmd.Parameters.AddWithValue("@UserPassword", password); 
+
+                        object userType = cmd.ExecuteScalar();
+
+                        if (userType != null)
+                        {
+                            authenticated = true;
+                            string role = userType.ToString();
+
+                            Session["UserType"] = role;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                   
+                    authenticated = false;
+                   
+                }
+            }
+
+            return authenticated;
         }
     }
 }
